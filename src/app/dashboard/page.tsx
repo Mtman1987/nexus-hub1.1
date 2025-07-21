0677f66ba61ccdcf2c65de68848b5a88e5109a18
Mt Man <mtman1987@gmail.com>
2025-07-21 18:16:56 +0000

    this was an issue last time can you see how much the boxes overlap, if y

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -253,7 +253,7 @@ export default function DashboardPage() {
                 {allToolModules
                     .filter(m => visibleModules[m.id] && !poppedOutModules.includes(m.id))
                     .map(m => (
-                    <div key={m.id} className="min-h-[400px] relative">
+                    <div key={m.id} className="h-[470px] relative">
                         {React.createElement(m.component, {
                             isPoppedOut: false,
                         })}
