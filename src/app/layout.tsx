61bb96793fb951124dbdf3e6fb6deecd98620bbb
Mt Man <mtman1987@gmail.com>
2025-07-22 02:51:55 +0000

    so how many divs total im just gathering data so i can use the proper te

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,13 +33,16 @@ export default function RootLayout({
            <div className="shooting-star" style={{ top: '10vh', left: '40vw', animationDelay: '-1s' }}></div>
            <div className="shooting-star" style={{ top: '50vh', left: '60vw', animationDelay: '-3.4s' }}></div>
            <div className="shooting-star" style={{ top: '80vh', left: '90vw', animationDelay: '-5.8s' }}></div>
+           <div className="shooting-star-reverse" style={{ top: '20vh', left: '10vw', animationDelay: '-2.2s' }}></div>
+           <div className="shooting-star-reverse" style={{ top: '60vh', left: '80vw', animationDelay: '-4.1s' }}></div>
+           <div className="shooting-star-reverse" style={{ top: '90vh', left: '30vw', animationDelay: '-0.5s' }}></div>
         </div>
         <LogProvider>
           <BotNameProvider>
             <SidebarProvider>
               <div className="flex min-h-screen w-full">
                 <Sidebar />
-                <main className="flex-1 flex flex-col overflow-auto bg-transparent">
+                <main className="flex-1 flex flex-col overflow-auto bg-background">
                   <header className="flex h-14 items-center gap-4 border-b bg-card/80 px-4 md:hidden">
                     <MobileSidebar />
                     <h1 className="text-lg font-bold">Apollo Station</h1>
