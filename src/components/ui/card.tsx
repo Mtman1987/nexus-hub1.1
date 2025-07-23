a7586d686d6ed0e25852791fd4c0ed2d2cf2e850
Mt Man <mtman1987@gmail.com>
2025-07-23 08:28:21 +0000

    yes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-neutral-700 bg-neutral-900",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
@@ -51,7 +51,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-neutral-400", className)}
+    className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
 ))
