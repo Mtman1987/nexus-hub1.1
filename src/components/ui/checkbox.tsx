d204effa40d713fa0eca7f8181a5f596ab38e516
Mt Man <mtman1987@gmail.com>
2025-07-23 09:27:05 +0000

    dont forget switches as well and then you may proceed

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -21,7 +22,7 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-4 w-4 text-[hsl(var(--accent-foreground))]" />
+      <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
