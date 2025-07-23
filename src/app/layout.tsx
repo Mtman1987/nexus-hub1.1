628dd65a14dfe1c8624d348598e7643d82fac7c1
Mt Man <mtman1987@gmail.com>
2025-07-23 04:22:14 +0000

    nope ok i want you to go through ever folder every file every line of co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,19 +23,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#000000" />
       </head>
-      <body className="font-body antialiased text-foreground flex h-screen w-screen overflow-hidden bg-background">
+      <body>
         <LogProvider>
           <BotNameProvider>
             <SidebarProvider>
               <ControlPanelProvider>
-                <div className="flex flex-1">
+                <div className="flex flex-1 h-screen">
                     <Sidebar />
                     <div className="flex flex-col flex-1 h-screen overflow-hidden">
                        <Header />
