e89bdc712e5ceeca93b2b62bea6a2e69e0a32d7d
Mt Man <mtman1987@gmail.com>
2025-07-24 11:58:23 +0000

    you cant tell in this photo but im hovering and even selevting  mountain

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -87,7 +87,7 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
+          "p-1 bg-secondary",
           position === "popper" &&
             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
